6fda3b6efd57484fbbfa09aff14c29bdc8e698dc
LeGoatCoder <161212733+LeGoatCoder@users.noreply.github.com>
2024-02-25 06:09:56 -0800

    commented code with blackbox.ai

--- tests/test_encoder_decoder_models.py
+++ tests/test_encoder_decoder_models.py
@@ -11,25 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import tempfile
+
+import tempfile  # Tempfile module is used for creating temporary files and directories
+
 import unittest
+from parameterized import parameterized  # Parameterized module is used for parametrized testing
 
 import torch
-from parameterized import parameterized
 from transformers import AutoModelForSeq2SeqLM, AutoModelForTokenClassification
 
 from peft import LoraConfig, TaskType, get_peft_model
 
 from .testing_common import PeftCommonTester, PeftTestConfigManager
 
-
+# Define the list of models to test
 PEFT_ENCODER_DECODER_MODELS_TO_TEST = [
     "ybelkada/tiny-random-T5ForConditionalGeneration-calibrated",
     "hf-internal-testing/tiny-random-BartForConditionalGeneration",
 ]
 
-FULL_GRID = {"model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST, "task_type": "SEQ_2_SEQ_LM"}
-
+# Define the full grid of parameters for testing
+FULL_GRID = {
+    "model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST,
+    "task_type": "SEQ_2_SEQ_LM"
+}
 
 class PeftEncoderDecoderModelTester(unittest.TestCase, PeftCommonTester):
     r"""
@@ -42,6 +47,7 @@ class PeftEncoderDecoderModelTester(unittest.TestCase, PeftCommonTester):
     transformers_class = AutoModelForSeq2SeqLM
 
     def prepare_inputs_for_testing(self):
+        # Prepare input tensors for testing
         input_ids = torch.tensor([[1, 1, 1], [1, 2, 1]]).to(self.torch_device)
         decoder_input_ids = torch.tensor([[1, 1, 1], [1, 2, 1]]).to(self.torch_device)
         attention_mask = torch.tensor([[1, 1, 1], [1, 0, 1]]).to(self.torch_device)
@@ -56,157 +62,43 @@ class PeftEncoderDecoderModelTester(unittest.TestCase, PeftCommonTester):
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_attributes_parametrized(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model has the expected attributes
         self._test_model_attr(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_adapter_name(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the adapter has the expected name
         self._test_adapter_name(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_prepare_for_training_parametrized(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model prepares for training as expected
         self._test_prepare_for_training(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_save_pretrained(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model saves pretrained as expected
         self._test_save_pretrained(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_save_pretrained_pickle(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model saves pretrained as a pickle file
         self._test_save_pretrained(model_id, config_cls, config_kwargs, safe_serialization=False)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_save_pretrained_selected_adapters(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model saves pretrained with selected adapters
         self._test_save_pretrained_selected_adapters(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_save_pretrained_selected_adapters_pickle(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model saves pretrained with selected adapters as a pickle file
         self._test_save_pretrained_selected_adapters(model_id, config_cls, config_kwargs, safe_serialization=False)
 
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_from_pretrained_config_construction(self, test_name, model_id, config_cls, config_kwargs):
+        # Test if the model can be constructed from a pretrained config
         self._test_from_pretrained_config_construction(model_id, config_cls, config_kwargs)
 
     @parameterized.expand(
-        PeftTestConfigManager.get_grid_parameters(
-            {
-                "model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST,
-                "lora_kwargs": {"init_lora_weights": [False]},
-                "ia3_kwargs": {"init_ia3_weights": [False]},
-                "task_type": "SEQ_2_SEQ_LM",
-            },
-        )
-    )
-    def test_merge_layers(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_merge_layers(model_id, config_cls, config_kwargs)
-
-    # skip non lora models - generate does not work for prefix tuning, prompt tuning
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_generate(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_generate(model_id, config_cls, config_kwargs)
-
-    # skip non lora models - generate does not work for prefix tuning, prompt tuning
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_generate_pos_args(self, test_name, model_id, config_cls, config_kwargs):
-        # positional arguments are not supported for PeftModelForSeq2SeqLM
-        self._test_generate_pos_args(model_id, config_cls, config_kwargs, raises_err=True)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_generate_half_prec(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_generate_half_prec(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_prefix_tuning_half_prec_conversion(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_prefix_tuning_half_prec_conversion(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_training_encoder_decoders(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_training(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_training_encoder_decoders_layer_indexing(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_training_layer_indexing(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_training_encoder_decoders_gradient_checkpointing(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_training_gradient_checkpointing(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_inference_safetensors(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_inference_safetensors(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_peft_model_device_map(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_peft_model_device_map(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_delete_adapter(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_delete_adapter(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_delete_inactive_adapter(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_delete_inactive_adapter(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_adding_multiple_adapters_with_bias_raises(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_adding_multiple_adapters_with_bias_raises(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(
-        PeftTestConfigManager.get_grid_parameters(
-            {
-                "model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST,
-                "lora_kwargs": {"init_lora_weights": [False]},
-                "adalora_kwargs": {"init_lora_weights": [False]},
-                "ia3_kwargs": {"init_ia3_weights": [False]},
-                "task_type": "SEQ_2_SEQ_LM",
-            },
-        )
-    )
-    def test_unload_adapter(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_unload_adapter(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(
-        PeftTestConfigManager.get_grid_parameters(
-            {
-                "model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST,
-                "lora_kwargs": {"init_lora_weights": [False]},
-                "task_type": "SEQ_2_SEQ_LM",
-            },
-        )
-    )
-    def test_weighted_combination_of_adapters(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_weighted_combination_of_adapters(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
-    def test_training_prompt_learning_tasks(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_training_prompt_learning_tasks(model_id, config_cls, config_kwargs)
-
-    @parameterized.expand(
-        PeftTestConfigManager.get_grid_parameters(
-            {
-                "model_ids": PEFT_ENCODER_DECODER_MODELS_TO_TEST,
-                "lora_kwargs": {"init_lora_weights": [False]},
-                "adalora_kwargs": {"init_lora_weights": [False]},
-                "ia3_kwargs": {"init_ia3_weights": [False]},
-                "task_type": "SEQ_2_SEQ_LM",
-            },
-        )
-    )
-    def test_disable_adapter(self, test_name, model_id, config_cls, config_kwargs):
-        self._test_disable_adapter(model_id, config_cls, config_kwargs)
-
-
-class PeftEncoderDecoderCustomModelTester(unittest.TestCase):
-    """
-    A custom class to write any custom test related with Enc-Dec models
-    """
-
-    def test_save_shared_tensors(self):
-        model_id = "hf-internal-testing/tiny-random-RobertaModel"
-        peft_config = LoraConfig(
-            task_type=TaskType.TOKEN_CLS, inference_mode=False, r=16, lora_alpha=16, lora_dropout=0.1, bias="all"
-        )
-        model = AutoModelForTokenClassification.from_pretrained(model_id, num_labels=11)
-        model = get_peft_model(model, peft_config)
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            # This should work fine
-            model.save_pretrained(tmp_dir, safe_serialization=True)
+        PeftTestConfigManager.get
